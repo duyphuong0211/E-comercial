805ab04bffae7b18aa1a25843c9e08a0d20b808d
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-08-22 20:37:54 +0700

    init

---
+++ src/main/java/com/Ecomercial/CTTT2018/Cttt2018Application.java
@@ -0,0 +1,13 @@
+package com.Ecomercial.CTTT2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cttt2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cttt2018Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ecomercial/CTTT2018/Cttt2018ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecomercial.CTTT2018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cttt2018ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
