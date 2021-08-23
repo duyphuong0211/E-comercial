5506504abe4d967140da3e3c6d0e9bf858be520a
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-08-23 15:55:45 +0700

    Day2: Authentication (Session Based)

--- src/main/java/com/Ecomercial/CTTT2018/controller/UserController.java
+++ src/main/java/com/Ecomercial/CTTT2018/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.Ecomercial.CTTT2018.controller;
 
+import com.Ecomercial.CTTT2018.auth.Authenticator;
 import com.Ecomercial.CTTT2018.entity.User;
 import com.Ecomercial.CTTT2018.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,27 +20,27 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/login",method = RequestMethod.GET)
-	public String login()
-	{
+	@Autowired
+	private Authenticator authenticator;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
 		return "user/login";
 	}
 
-	@RequestMapping(value= "/login",method = RequestMethod.POST)
-	public String login(HttpServletRequest request, Model model)
-	{
-		String username=request.getParameter("username");
-		String password=request.getParameter("password");
-
-		if(userService.login(username, password))
-		{
-			//TODO SET SESSIONS OR WHTVR TO MAKE U LOGIN
-			model.addAttribute("username",username);
-			model.addAttribute("title",username);
-			return "user/profile";
-		}
-		else {
-			model.addAttribute("message","Wrong Username or password");
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(HttpServletRequest request, Model model) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (userService.login(username, password)) {
+			//Save New Session
+			//TODO userID
+			authenticator.saveAuth(1337, username);
+			model.addAttribute("username", username);
+			model.addAttribute("title", username);
+			return "redirect:/";
+		} else {
+			model.addAttribute("message", "Wrong Username or password");
 			return "user/login";
 		}
 	}
@@ -51,13 +53,26 @@ public class UserController {
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String register(@ModelAttribute User user, HttpServletRequest request, Model model) {
 		String confirmPassword = request.getParameter("confirmPassword");
-		if(userService.register(user, confirmPassword))
-		{
-			//TODO FFS THIS SPRING SHIT IS FUCKING RETARDED IT REDIRECTS USING CONTEXT PATH FFFSFS!#$!#
-			return "redirect: .../";
+		if (userService.register(user, confirmPassword)) {
+			//SAVE SESSION
+			//TODO userID
+			authenticator.saveAuth(1337,user.getUsername());
+			return "redirect:/user/view/"+ user.getUsername();
 		}
-		model.addAttribute("message","Failed to Register, hanl2y tare2a ngeb howa failed leh bezabt ba3den");
+		model.addAttribute("message", "Failed to Register, hanl2y tare2a ngeb howa failed leh bezabt ba3den");
 		return "user/register";
+	}
+
+	@RequestMapping(value = "/logout")
+	public String logout() {
+		//Delete Session
+		authenticator.removeAuth();
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/view/{username}")
+	public String view(@PathVariable("username") String username) {
+		return "user/profile";
 	}
 
 }
