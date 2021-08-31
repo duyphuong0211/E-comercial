817c555d48a57b33614bdd37d3131cc4595a31cf
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-08-31 15:44:52 +0700

    Day7: Update new CurrentUser to reglect roles changes

--- src/main/java/com/Ecomercial/CTTT2018/auth/CurrentUser.java
+++ src/main/java/com/Ecomercial/CTTT2018/auth/CurrentUser.java
@@ -1,28 +1,34 @@
 package com.Ecomercial.CTTT2018.auth;
 
+import antlr.StringUtils;
 import com.Ecomercial.CTTT2018.models.domain.Role;
 import com.Ecomercial.CTTT2018.models.domain.User;
 import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
 
 public class CurrentUser extends org.springframework.security.core.userdetails.User {
 
 	private User user;
 
 	public CurrentUser(User user) {
-		super(user.getUsername(), user.getPasswordHash(), AuthorityUtils.createAuthorityList(user.getRole().toString()));
+		super(user.getUsername(), user.getPasswordHash(),
+				AuthorityUtils.commaSeparatedStringToAuthorityList(
+						StringUtils.stripFrontBack(user.getRoles().toString(), "[", "]" )
+				)
+		);
 		this.user = user;
 	}
 
 	public User getUser() {
 		return user;
 	}
-
 	public long getId() {
 		return user.getId();
 	}
 
-	public Role getRole() {
-		return user.getRole();
+	public Collection<Role> getRole() {
+		return user.getRoles();
 	}
 
 }
