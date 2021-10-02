54dece33c993b639543ab9d46dc60f1a43e5d70a
duyphuong0211 <18521276@gm.uit.edu.vn>
2021-10-02 16:01:42 +0700

    Day 12:Added Charts!

--- src/main/java/com/Ecomercial/CTTT2018/viewmodels/StoreOwnerDashboardViewModel.java
+++ src/main/java/com/Ecomercial/CTTT2018/viewmodels/StoreOwnerDashboardViewModel.java
@@ -7,22 +7,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @Component
 public class StoreOwnerDashboardViewModel {
-
     @Autowired
     StoreService storeService;
-
     //Get User Stores.
     public HashMap<String, Object> create(Long Id) {
         HashMap<String, Object> model = new HashMap<>();
         Collection<Store> Accepted=storeService.getAllAcceptedUserStores(Id);
         Collection<Store> Pending=storeService.getAllPendingUserStores(Id);
         Collection<Store> Rejected=storeService.getAllNotAcceptedUserStores(Id);
+
+        //statistics charts data
+        Collection<String> Names         = Accepted.stream().map(Store::getName).collect(Collectors.toList());
+        //Collection<Integer> ProductsCount = Accepted.stream().map(x -> x.getStoreProducts().size()).collect(Collectors.toList());
+        Collection<Integer> ProductsCount = Accepted.stream().map(x -> x.getStoreProducts().stream().map(y -> y.getStoreViews()).reduce(0, (a,b) -> a + b)).collect(Collectors.toList());
+
         model.put("accepted",Accepted);
         model.put("pending",Pending);
         model.put("rejected",Rejected);
+
+        model.put("names",Names);
+        model.put("productCount",ProductsCount);
         return model;
     }
 
